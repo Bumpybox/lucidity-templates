769dbbfdb96dbc785f428726d4eea8e88dfc1e79
Toke Jepsen <toke.jepsen@baitstudio.local>
2016-06-30 13:38:23 +0100

    snapshot commit

--- pipeline_schema/utils.py
+++ pipeline_schema/utils.py
@@ -1,4 +1,12 @@
 import os
+import json
+import base64
+import sys
+import uuid
+import traceback
+import re
+
+sys.path.insert(0, r'K:\development\tools\lucidity\source')
 
 import lucidity
 
@@ -6,11 +14,28 @@ import lucidity
 def get_ftrack_data(entityId):
     import ftrack
 
+    # if in a connect session, we can get the entityId from the connect event
+    if 'FTRACK_CONNECT_EVENT' in os.environ and not entityId:
+        decodedEventData = json.loads(
+            base64.b64decode(
+                os.environ.get('FTRACK_CONNECT_EVENT')
+            )
+        )
+
+        entityId = decodedEventData['selection'][0]['entityId']
+
     entity = None
+
+    # resolving the entityId to a ftrack entity
     try:
         entity = ftrack.Task(entityId)
         if entity.getObjectType() != 'Task':
             entity = None
+    except:
+        pass
+
+    try:
+        entity = ftrack.Component(entityId)
     except:
         pass
 
@@ -19,51 +44,107 @@ def get_ftrack_data(entityId):
 
     parents = entity.getParents()
     project = parents[-1]
+    data = dict()
 
-    path = []
-    asset_build = None
-    for parent in parents[:-1]:
-        path.append(parent.getName())
+    # project root
+    root = project.getRoot()
+    if root.endswith('\\'):
+        root = root[:-1]
+    data['project_root'] = root
 
-        if parent.getObjectType() == 'Asset Build':
-            asset_build = parent
+    # getting task data
+    if entity.get('entityType') == 'task':
 
-    if len(parents) > 1:
-        path.append(parents[-2].getObjectType().lower() + 's')
+        path = []
+        asset_build = None
+        for parent in parents[:-1]:
+            path.append(parent.getName())
 
-    path.append(project.getRoot())
-    path.reverse()
-    root = os.path.join(*path).lower()
+            if parent.getObjectType() == 'Asset Build':
+                asset_build = parent
+        path.reverse()
 
-    data = {'name': entity.getParent().getName().lower(),
-            'task_name': entity.getName().lower(),
-            'root': root}
+        try:
+            data['parent_path'] = os.path.join(*path)
+        except:
+            data['parent_path'] = ''
 
-    # if its an asset
-    if asset_build:
-        data['asset_type'] = asset_build.getType().getName().lower()
-        data['asset_name'] = asset_build.getName().lower()
-        data['root'] = project.getRoot()
+        try:
+            data['parent_types'] = parents[-2].getObjectType() + 's'
+        except:
+            data['parent_types'] = ''
+
+        data['task_name'] = entity.getName()
+        data['name'] = entity.getParent().getName()
+
+        # if its an asset
+        if asset_build:
+            data['asset_type'] = asset_build.getType().getName()
+            data['asset_name'] = asset_build.getName()
+            data['root'] = project.getRoot()
+
+    # getting component data
+    if entity.get('entityType') == 'component':
+
+        asset = entity.getVersion().getAsset()
+        data['output_type'] = asset.getType().getShort()
+
+        path = []
+        for parent in parents[2:-1]:
+            path.append(parent.getName())
+        path.reverse()
+
+        data['parent_path'] = os.path.join(*path)
+        data['task_name'] = entity.getVersion().getTask().getName()
+        data['name'] = entity.getName()
 
     return data
 
 
-def get_path(path_type, data, version, extension):
+def get_data(item_id=None):
+
+    data = dict()
+
+    try:
+        data = get_ftrack_data(item_id)
+    except:
+        print traceback.format_exc()
+
+    data['user_directory'] = os.path.expanduser("~")
+    data['uuid'] = str(uuid.uuid4())
+
+    # version
+    data['version'] = data.get('version', 1)
+
+    return data
+
+
+def get_path(path_type, data):
     schema_path = os.path.join(os.path.dirname(__file__), 'schema.yml')
     schema = lucidity.Schema.from_yaml(schema_path)
 
-    # non ftrack data
-    data['version'] = version
-    data['filetype'] = extension
+    # required format of version item
+    if isinstance(data['version'], int):
+        data['version'] = str(data['version']).zfill(3)
+
+    # checking all required data exists
+    missing_keys = list(schema[path_type].keys() - set(data.keys()))
+    if len(missing_keys) != 0:
+        raise ValueError('Missing keys in data: %s' % missing_keys)
 
     paths = {}
     for fm in schema.format_all(data):
         paths[fm[1].name] = fm[0]
 
-    return paths[path_type].replace('\\', '/')
+    # removing spaces and backslashes
+    result = paths[path_type].replace(' ', '_').replace('\\', '/')
+
+    # removing multiple forward slashes
+    result = re.sub("/{2,}", "/", result)
+
+    return result.lower()
 
 if __name__ == "__main__":
-    data = get_ftrack_data('06499796-1e77-11e6-a1d5-42010af00048')
-    version = str(1).zfill(3)
-    extension = 'scn'
-    print get_path('task_work', data, version, extension)
+
+    data = get_data()
+    print get_path('version', data)

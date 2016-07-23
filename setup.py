66324d1248ece0962c30373f6418518d670a7a43
Toke Jepsen <tokejepsen@gmail.com>
2016-07-23 17:13:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pipeline-schema',
+      version='0.1',
+      description='Bumpybox pipeline schema',
+      url='https://github.com/Bumpybox/pipeline-schema',
+      author='Toke Jepsen',
+      author_email='tokejepsen@bumpybox.com',
+      license='MIT',
+      packages=['pipeline_schema'],
+      zip_safe=False)
